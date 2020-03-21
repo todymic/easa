75baa62571c29e533d5c5220e5bf2fa38836bca0
todymic <gtody.rabekoto@gmail.com>
2020-03-21 23:51:30 +0100

    Page Register

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,7 +3,11 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +16,53 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
+            ->add('lastname', null, [
+                'attr' => [
+                    'placeholder' => 'form.input.lastname'
+                ]
+            ])
+            ->add('firstname', null,[
+                'attr' => [
+                    'placeholder' => 'form.input.firstname'
+                ]
+            ])
+            ->add('adresse', null,[
+                'attr' => [
+                    'placeholder' => 'form.input.adresse'
+                ]
+            ])
+            ->add('city', null,[
+                'attr' => [
+                    'placeholder' => 'form.input.city'
+                ]
+            ])
+            ->add('country', null,[
+                'attr' => [
+                    'placeholder' => 'form.input.country'
+                ]
+            ])
+            ->add('phone', null,[
+                'attr' => [
+                    'placeholder' => 'form.input.phone'
+                ]
+            ])
+            ->add('email', EmailType::class,[
+                'attr' => [
+                    'placeholder' => 'form.input.email'
+                ]
+            ])
+            ->add('password', PasswordType::class,[
+                'attr' => [
+                    'placeholder' => 'form.input.firstname'
+                ]
+            ])
+            ->add('roles', ChoiceType::class, [
+                'multiple' => true,
+                'expanded' => true, // render check-boxes
+                'choices' => [
+                    'Admin' => 'ROLE_ADMIN'
+                ],
+            ])
         ;
     }
 
